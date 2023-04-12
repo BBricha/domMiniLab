b46475321a44a6f010ceb5ed17bd0ea4b9ccbf95
Bader Bricha <bbricha@LABA.PSWAS.LOCAL>
2023-04-12 15:57:05 -0700

    finish at home

--- domMiniLab.js
+++ domMiniLab.js
@@ -1,3 +1,10 @@
 
+let h1 = document.querySelector('h1')
+h1.textContent = 'some text'
 
-
+let hideButton = document.getElementById('hide-button')
+hideButton.addEventListener('click', () => {
+    let h1 = document.querySelector('h1')
+    h1.style.display = 'none'
+}
+)
